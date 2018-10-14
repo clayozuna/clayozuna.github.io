583df2534829d26b16802c5955faf529657fd713
Clay Edward Ozuna <ozunaclay@gmail.com>
2018-10-14 01:04:58 -0500

    Add php folder and UTF-8

---
+++ php/response.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>Clay E. Ozuna</title>
+    <link href="./css/about.css" type="text/css" rel="stylesheet">
+	<link href="//fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="Clay E. Ozuna">
+    <meta charset="UTF-8">
+    <meta name="description" content="Howdy! I'm Clay Ozuna and this website is my professional portfolio.
+  Feel free to explore and learn about my projects, organizations, and hobbies.">
+</head>
+
+<body>
+  Thanks for your resonse <?php echo $_GET["name"]; ?>
+</body>
+</html>
