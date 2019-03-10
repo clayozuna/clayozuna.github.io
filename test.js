44e947449fd09449c9545a8d9bd6c3c8002c4c38
Clay Edward Ozuna <ozunaclay@gmail.com>
2019-03-10 16:56:30 -0500

    Change weather to Fahrenheit

--- test.js
+++ test.js
@@ -29,7 +29,7 @@ function theResponse(response) {
   let jsonObject = JSON.parse(response);
   cityName.innerHTML = jsonObject.name;
   icon.src = "http://openweathermap.org/img/w/" + jsonObject.weather[0].icon + ".png";
-  temperature.innerHTML = parseInt(jsonObject.main.temp - 273) + "°";
+  temperature.innerHTML = parseInt(jsonObject.main.temp - 273.15 * (9/5) + 32) + "°F";
   humidity.innerHTML = jsonObject.main.humidity + "%";
 }
 
